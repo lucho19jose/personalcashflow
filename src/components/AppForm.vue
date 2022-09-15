429cadc0ba35a2706d34fc9bb372be2d9836dc22
lucho19jose <barbozagonzalesjose@gmail.com>
2022-09-15 13:12:58 -0500

    app form

---
+++ src/components/AppForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="text-h6" style="text-align:center;">Añadir nuevo {{ props.name }}</div>
+    <div class="q-mb-sm">
+      <q-input outlined type="number" v-model="amount" label="Monto" />
+    </div>
+    <div v-if="props.category" class="q-mb-sm">
+      <q-select outlined v-model="categoryselected" :options="categories" label="Categoria" />
+    </div>
+    <div class="q-mb-sm">
+      <q-input outlined v-model="description" label="Descripcion" />
+    </div>
+    <div class=" row justify-center">
+      <q-btn color="primary" :label="`Añadir ${ props.name }`" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const amount = ref<number>(0);
+  const description = ref<string>('');
+  const categories = ref<string[]>(['Alimentacion', 'Pasaje', 'Estudio', 'Diversion', 'Otro']);
+  const categoryselected = ref<string>('');
+
+  const props = defineProps({
+    name: { type: String, required: true },
+    category: { type: Boolean, default: true },
+
+  })
+</script>
+<style scoped>
+  .btn-add {
+   width: 100%;
+  }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,48 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div>
-      it is already loaded.
+  <q-page padding class="row justify-evenly">
+    <div style="width: 100%;">
+      <q-card style="width: 100%">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="expenses" label="Gasto" />
+          <q-tab name="income" label="Ingreso" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="expenses">
+            <!-- Un gasto o egreso es el consumo de un bien o servicio a cambio de una contraprestación, que suele hacerse efectiva mediante un pago monetario. -->
+            <AppForm
+              :name="'Gasto'"
+            ></AppForm>
+          </q-tab-panel>
+
+          <q-tab-panel name="income">
+            <AppForm
+              :name="'Ingreso'"
+              :category="false"
+            ></AppForm>
+          </q-tab-panel>
+
+        </q-tab-panels>
+      </q-card>
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import AppForm from 'src/components/AppForm.vue';
+  
+  const tab = ref<string>('expenses'); 
+
 
 </script>
